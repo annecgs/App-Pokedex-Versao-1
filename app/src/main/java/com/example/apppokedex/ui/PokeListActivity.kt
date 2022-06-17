8098a9b99586c24d511e0cb669340874ebaed9f0
Ana Gomes Souza <agomesou@emeal.nttdata.com>
2022-06-17 11:38:52 -0300

    Incluindo layout

--- app/src/main/java/com/example/apppokedex/ui/PokeListActivity.kt
+++ app/src/main/java/com/example/apppokedex/ui/PokeListActivity.kt
@@ -2,6 +2,7 @@ package com.example.apppokedex.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.apppokedex.R
 
 class PokeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
