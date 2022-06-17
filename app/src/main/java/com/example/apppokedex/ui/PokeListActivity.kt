fa3346de99ac1985df3d9b9b39340e42177b0d8c
Ana Gomes Souza <agomesou@emeal.nttdata.com>
2022-06-17 11:38:46 -0300

    Incluindo layout

---
+++ app/src/main/java/com/example/apppokedex/ui/PokeListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.apppokedex.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokeListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poke_list)
+    }
+}
